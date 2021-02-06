e65b1f7e151e84a2860eb2ea26febbeb7d7f0094
khaalid subaan <khaalidsubaan@gmail.com>
2021-02-06 22:29:53 +0800

    image link

--- util/index.ts
+++ util/index.ts
@@ -3,3 +3,7 @@ import Axios from "axios";
 export const axios = Axios.create({
   baseURL: " https://jsonplaceholder.typicode.com",
 });
+
+export const imageApi = Axios.create({
+  baseURL: "https://picsum.photos/v2list?page=1&limit=20",
+});
