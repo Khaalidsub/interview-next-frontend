61d40a3d5a075f8dc78557ff00e8dbc383f4f505
khaalid subaan <khaalidsubaan@gmail.com>
2021-02-07 04:34:34 +0800

    margin fix on the nav

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -2,8 +2,8 @@
 
 export default function Navigation({ children }) {
     return (
-        <div className="my-12 flex justify-center">
-            <div className="object-center border border-black shadow-lg max-w-xl flex-grow py-4 rounded-md"  >
+        <div className="my-12 mx-4 flex justify-center">
+            <div className="object-center border border-black shadow-md max-w-xl flex-grow py-4 rounded-md"  >
 
 
 
